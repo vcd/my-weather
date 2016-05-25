2e69de4eb4b220caa099bef9779c0b178f96d52e
ewuweblab <pmanikoth@engr-131-inst.eastern.ewu.edu>
2016-05-25 16:09:48 -0700

    Added Forecast

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,32 @@ $.simpleWeather({
       $('#cheney .temp').text(weather.temp);
       // Display Icon
       $('#cheney i').addClass( 'icon-' + weather.code );        
+      // Get Forecast
+      $('#d1').text(weather.forecast[1].date);
+      $('#d1').text(weather.forecast[1].high);
+      $('#d1').text(weather.forecast[1].code);
+        
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+    }
+  
+  });
+
+// Get Spokane Weather
+$.simpleWeather({
+    location: '99203',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('#spokane .city').text(weather.city);
+      $('#spokane .temp').text(weather.temp);
+      // Display Icon
+      $('#spokane i').addClass( 'icon-' + weather.code );        
     
       // Entire weather object
       console.log(weather);
@@ -36,3 +62,60 @@ $.simpleWeather({
     }
   
   });
+
+// Get Geo Location
+// Check for GeoLocation Support on Browser
+if ('geolocation' in navigator) {
+
+   $('.geo button').show(); 
+
+} else {
+  
+  $('.geo button').hide();
+  $('.geo').prepend('<p>Geolocation Not Supported</p>');
+
+}
+
+// On Click, Get Geolocation, Call Weather Function
+$('.geo button').click( function() {
+     
+    //load weather using your lat/lng coordinates
+    navigator.geolocation.getCurrentPosition(function(position) {
+        getWeather(position.coords.latitude+','+position.coords.longitude); 
+    });
+   
+});
+
+// Get geolocated weather
+var getWeather = function(location) {
+    
+   $.simpleWeather({
+    location: location,
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.geo .temp').text(weather.temp);
+      $('.geo .city').text(weather.city);
+    // Display Icon
+      $('.geo i').addClass( 'icon-' + weather.code );    
+        
+      // Entire weather object
+      console.log();
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+    }
+  
+  });
+    
+};
+
+
+
+
+
+
+
+
